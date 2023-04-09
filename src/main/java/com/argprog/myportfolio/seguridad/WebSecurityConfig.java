04c81277b7e8838823cf0789f85f020a86a0d1e3
Tomas Centurion <108957807+TomasCentu@users.noreply.github.com>
2023-04-09 15:07:36 -0300

    ##

--- src/main/java/com/argprog/myportfolio/seguridad/WebSecurityConfig.java
+++ src/main/java/com/argprog/myportfolio/seguridad/WebSecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,7 +18,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import com.argprog.myportfolio.servicios.UserDetailsImpServ;
 
 @Configuration
-@EnableMethodSecurity
 public class WebSecurityConfig {
 
   @Autowired
